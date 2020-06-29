b195d3c0daba4293f347416377df3bd5aba6c205
DabulKaddes <54689186+DabulKaddes@users.noreply.github.com>
2020-06-30 00:37:54 +0300

    Add files via upload

--- Dabul.lua
+++ Dabul.lua
@@ -3563,11 +3563,11 @@ end
 
 if text == ("كتم") and msg.reply_to_message_id_ ~= 0 and Addictive(msg) then  
 if AddChannel(msg.sender_user_id_) == false then
-local textchuser = database:get(bot_id..'text:ch:user')
+local textchuser = database:get('DabulKaddes:'..bot_id..'text:ch:user')
 if textchuser then
 send(msg.chat_id_, msg.id_,'['..textchuser..']')
 else
-send(msg.chat_id_, msg.id_,'👥┇ لا تستطيع استخدام البوت يرجى الاشتراك في القناة حتى تتمكن من استخدام الاوامر \n 📌┇ اشترك هنا ['..database:get(bot_id..'add:ch:username')..']')
+send(msg.chat_id_, msg.id_,'- عليك الاشتراك في قناة البوت اولآ 📄\n- لكي تتمكن من استخدام البوت📮\n- معرف القناة ['..database:get('DabulKaddes:'..bot_id..'add:ch:username')..']')
 end
 return false
 end
